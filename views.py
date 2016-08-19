093e29b00b16fc59058adca1f5998866566b589f
Denys Gorbach <denysg@opera.com>
2016-08-19 03:55:19 +0200

    fixing wrong commit and leaked credentials :)

--- views.py
+++ views.py
@@ -9,10 +9,12 @@ from functools import wraps
 from flask import Flask, flash, redirect, session, url_for, request, render_template, abort
 from app import app
 
+app.secret_key = app.config['SECRET_KEY']
+
 if app.debug is not True:
     import logging
     from logging.handlers import RotatingFileHandler
-    file_handler = RotatingFileHandler('/var/www/wikitranslate/log/error.log', maxBytes=1024 * 1024 * 100, backupCount=20)
+    file_handler = RotatingFileHandler('/srv/wikitranslate/log/error.log', maxBytes=1024 * 1024 * 100, backupCount=20)
     file_handler.setLevel(logging.ERROR)
     formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
     file_handler.setFormatter(formatter)
@@ -24,21 +26,24 @@ def index():
     input_lang = request.args.get('input-lang')
     input_text = request.args.get('input-text')
     output_lang = request.args.get('output-lang')
-    output_languages = None
+    output_languages = wiki.get_all_languages()
     translation = None
     input_wiki_link = None
     output_wiki_link = None
 
     try:
-        if input_lang and input_text:
+        if input_lang and input_text and output_lang:
+	    input_wiki_link = wiki._get_wiki_link(input_text, input_lang)
             output_languages = wiki.get_available_languages(input_text, input_lang)
-            input_wiki_link = wiki._get_wiki_link(input_text, input_lang)
-        if output_lang:
             translation = wiki.translate(input_text, input_lang, output_lang)
             output_wiki_link = wiki._get_wiki_link(translation, output_lang)
+    except wiki.OriginalPageNotFound:
+        flash('Could not find <b>%s</b> Wikipedia article for <b>%s</b>.' % (wiki._get_lang_by_code(input_lang), input_text))
+    except wiki.TranslationNotFound:
+	flash('Could not find translation in <b>%s</b>.<br>The second dropdown list is now updated with <b>%s</b> languages in which the translation is available.' % (wiki._get_lang_by_code(output_lang), len(output_languages)))
     except Exception as e:
         flash(e)
-
+    
     return render_template(
             'index.html',
             all_languages=wiki.get_all_languages(),
